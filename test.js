564b5d0dacaae3b6224a66aa46768aebb8c7c3f8
yezert <oodx@icloud.com>
2022-08-19 15:58:59 +0800

    test

---
+++ test.js
@@ -0,0 +1,17 @@
+// console.log(document.body);
+
+// document.body.addEventListener("onload", (e) => {document.addEventListener
+// 	console.log("foo------");
+// 	// select the h1 by a class, id, or even tag name
+// 	// in this example, i'm just selecting it by tag name
+// 	var h1 = document.querySelector("h1");
+
+// 	h1.innerText = "Correct Text";
+// });
+
+console.log("foo--4444----");
+var h1 = document.querySelector("h1");
+
+h1.innerText = "Correct Text";
+
+var a = document.getElementsByClassName("TOCHER-JS");
